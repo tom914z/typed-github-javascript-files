001d3df6a5ddee5bcf4b4dbbe86b1a472df63a75
tom914z <168499260+tom914z@users.noreply.github.com>
2024-08-28 09:03:58 -0700

    Create index3.js

---
+++ index3.js
@@ -0,0 +1,30 @@
+// Typed
+
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() { // Property 'getter'.
+        return this.radius * 2;
+        // We just take the radius value multiplied by 2.
+    }
+
+    set diameter(value) { // Property 'setter'.
+        this.radius = value / 2;
+        // Here we set a new radius value.
+    }
+
+    get area() { // Read-only
+        return Math.PI * (this.radius ** 2);
+    }
+}
+
+let c = new Circle(2);
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+c.diameter = 1.6;
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
